b41f377c342fe18e8c74ef277ae828c65cbbfbfa
flowili <florian101991@aol.com>
2020-10-10 15:11:16 +0200

    Feature Tasmota plugin initial commit

---
+++ src/dataAggregator/customerTasmota/pluginTasmota.py
@@ -0,0 +1,36 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# imports
+
+detected_device = False
+detected_device_ip = False
+detected_device_is_valid = False
+
+def init_dev_by_alias(search_alias):
+    '''
+    Init Device by alias
+    :param search_alias:
+    :return:
+    '''
+
+def get_dev_value():
+    '''
+    :return: returns the acutal emeter value
+    '''
+    return value
+
+
+def get_device_valid():
+    '''
+    false device is not usable
+    True device is ready to read the emeter values
+    '''
+    return detected_device_is_valid
+
+
+def get_device_ip():
+    '''
+    :return: the device IP adress
+    '''
+    return detected_device_ip
