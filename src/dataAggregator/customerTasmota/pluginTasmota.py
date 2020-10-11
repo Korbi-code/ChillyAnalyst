4e6b15e92d56e3a34701f4c8c75f3fb5884bc17f
flowili <florian101991@aol.com>
2020-10-11 13:17:15 +0200

    start new branch

--- src/dataAggregator/customerTasmota/pluginTasmota.py
+++ src/dataAggregator/customerTasmota/pluginTasmota.py
@@ -39,7 +39,6 @@ def init_dev_by_alias(search_alias):
         if dev.alias == search_alias:
             ip_to_return = ip
     detected_device_ip = ip_to_return
-    #test test test
 
 
     #TODO:
