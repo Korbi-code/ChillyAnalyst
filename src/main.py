b85e835a28fc63b8e625e59620e3cb62de96a3cc
Korbi <sven.a.korbmacher@gmail.com>
2021-03-15 18:57:34 +0100

    Filter read power values with entry in config numpy added to requirements

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ import configparser
 from pathlib import Path
 import sys
 import os
+import numpy as np
+import collections
 
 # Import Module
 sys.path.append(os.getcwd())
@@ -24,6 +26,7 @@ REPO_PATH = str((Path(sys.argv[0]).parents[1]).resolve())
 DataAggregator_object = 0
 TelegramHandler_object = False
 DataContainer_object = 0
+InputFilter_deque = 0
 _LOGGER = get_logger(__file__)
 
 # State machine params
@@ -47,6 +50,11 @@ def init():
     global DataContainer_object
     DataContainer_object = DataContainer()
 
+    global InputFilter_deque
+    read_successful, cfg_parameter = get_configuration("parameter")
+    InputFilter_deque = collections.deque(maxlen=int(cfg_parameter["filter_queue"]))
+    InputFilter_deque.append(0)
+
 
 def cyclic_telegram_handler():
     global TelegramHandler_object
@@ -61,21 +69,29 @@ def cyclic_telegram_handler():
 
 def cyclic_state_machine_handler():
     global TelegramHandler_object
+    global InputFilter_deque
 
     while 1:
         if DataAggregator_object.get_device_valid():
 
             # Get new value
             read_power_mw, read_power_mw_valid = DataAggregator_object.get_dev_value()
-            DataContainer_object.add_new_value(int(read_power_mw * PARAM_EMETER_PLUG_RESOLUTION))
-            _LOGGER.debug(read_power_mw)
 
             if not read_power_mw_valid:
                 TelegramHandler_object.send_message("Device returns invalid value")
+                value_ll = InputFilter_deque[-1]
+                InputFilter_deque.append(value_ll)
+
+            else:
+                InputFilter_deque.append(read_power_mw)
+
+            read_power_mw_mean = round(np.mean(InputFilter_deque))
+            DataContainer_object.add_new_value(int(read_power_mw_mean * PARAM_EMETER_PLUG_RESOLUTION))
+            _LOGGER.debug("RAW: " + str(read_power_mw) + " MEAN:" + str(read_power_mw_mean))
 
             # State Machine
             if cyclic_state_machine_handler.detection_state == 'IDLE':
-                if read_power_mw > PARAM_POWER_LOWER_LEVEL:
+                if read_power_mw_mean > PARAM_POWER_LOWER_LEVEL:
                     cyclic_state_machine_handler.sleep_time = PARAM_MEASURE_TICK_RATE
                     DataContainer_object.enable_acquisition()
                     cyclic_state_machine_handler.detection_state = 'MEASURE'
@@ -85,7 +101,7 @@ def cyclic_state_machine_handler():
                     _LOGGER.info("Start Detected - State Transition to MEASURE")
 
             elif cyclic_state_machine_handler.detection_state == 'MEASURE':
-                if read_power_mw <= PARAM_POWER_DEBOUNCE_LEVEL:
+                if read_power_mw_mean <= PARAM_POWER_DEBOUNCE_LEVEL:
                     if cyclic_state_machine_handler.debounce_timer < PARAM_DEBOUNCE_TICK_LIMIT:
                         cyclic_state_machine_handler.debounce_timer += 1
                         _LOGGER.info("Debouncing")
