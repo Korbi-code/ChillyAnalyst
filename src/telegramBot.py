d20855bdcff950e81edccb1936dba39dca9ea615
Korbi <sven.a.korbmacher@gmail.com>
2021-03-16 22:58:03 +0100

    Introduce user database handling with tinydb

--- src/telegramBot.py
+++ src/telegramBot.py
@@ -3,14 +3,15 @@
 
 # Import Modules
 import os
-import csv
 from telegram import Update, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
 import telegram
-from telegram.ext import (Updater, MessageHandler, Filters, CallbackContext, CallbackQueryHandler)
+from telegram.ext import (Updater, MessageHandler, Filters, CallbackContext, CallbackQueryHandler, ConversationHandler,
+                          CommandHandler)
 import configparser
 
 # Import Class
 from src.chillyLogger import *
+from src.userHandler import *
 
 # Define global REPO_PATH
 REPO_PATH = str((Path(sys.argv[0]).parents[1]).resolve())
@@ -27,21 +28,9 @@ TELEGRAM_CFG.read(configFilePath)
 class TelegramHandler:
     def __init__(self, token):
         self.token = token
-
-        self.subscribed_users_path = REPO_PATH + str(TELEGRAM_CFG.get('telegram', 'subscribed_users_path'))
-
-        self.subscribed_users_file = self.subscribed_users_path + \
-                                     str(TELEGRAM_CFG.get('telegram', 'subscribed_users_filename'))
         self.password = TELEGRAM_CFG.get('telegram', 'password')
-
         self.updater = False
         self.bot = False
-
-        Path(self.subscribed_users_path).mkdir(parents=True, exist_ok=True)
-        try:
-            open(str(self.subscribed_users_file), 'r')
-        except FileNotFoundError:
-            open(str(self.subscribed_users_file), "w")
 
     def start(self):
         # Create the Updater and pass it your bot's token.
@@ -73,48 +62,41 @@ class TelegramHandler:
             lastname = "None"
 
         # Register new user
-        if message == str(TELEGRAM_CFG.get('telegram', 'Password')):
-            self.handle_subscription_request(id)
+        if message == str(TELEGRAM_CFG.get('telegram', 'password')):
+            if not get_is_user_registered(id):
+                register_new_user(id, firstname, lastname)
 
         # Get alive message from bot
         if message == 'Status':
             update.message.reply_text('Still running!')
 
-    def handle_subscription_request(self, chat_id):
-        if os.path.exists(self.subscribed_users_file):
-            append_write = 'a'  # append if already exists
+        if message == 'Stumm':
+            update_user_entry(id, 'notification', False)
 
-            # if file exists check if user is already on the list
-            with open(self.subscribed_users_file, newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-                for row in reader:
-                    if int(chat_id) == int(row[0]):
-                        _LOGGER.debug("User already on list: " + str(chat_id))
-                        return False
-        else:
-            append_write = 'w'  # make a new file if not
+        if message == 'Laut':
+            update_user_entry(id, 'notification', True)
 
-        with open(self.subscribed_users_file, append_write, newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=' ',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow([chat_id])
-            _LOGGER.info("New user registered with chat_id: " + str(chat_id))
-            return True
+        if message == 'Expert':
+            update_user_entry(id, 'type', 'Expert')
 
-    def send_message(self, txt):
-        if os.path.exists(self.subscribed_users_file):
-            with open(self.subscribed_users_file, newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-                for row in reader:
-                    self.bot.send_message(row[0], txt)
-                    _LOGGER.debug("Send Message: " + str(txt))
+        if message == 'Basic':
+            update_user_entry(id, 'type', 'Basic')
+
+    def send_message(self, txt, level='Basic'):
+        active_users = get_all_active_users()
+        if active_users:
+            for user in active_users:
+                if user['notification']:
+                    if level == user['type'] or level == 'Basic':
+                        self.bot.send_message(user['id'], txt)
+                        _LOGGER.debug("Send Message: " + str(txt))
 
     def send_html(self, png_path):
-        if os.path.exists(self.subscribed_users_file):
-            with open(self.subscribed_users_file, newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-                for row in reader:
-                    self.bot.send_document(row[0], document=open(png_path, 'rb'))
+        active_users = get_all_active_users()
+        if active_users:
+            for user in active_users:
+                if user['notification']:
+                    self.bot.send_document(user['id'], document=open(png_path, 'rb'))
                     _LOGGER.debug("Send HTML: " + str(png_path))
 
     def send_user_question(self):
@@ -128,15 +110,13 @@ class TelegramHandler:
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
-        if os.path.exists(self.subscribed_users_file):
-            with open(self.subscribed_users_file, newline='') as csvfile:
-                reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-                for row in reader:
-                    self.bot.send_message(row[0], 'Wird der Waschvorgang länger oder kürzer als eine Stunde laufen ?',
-                                          reply_markup=reply_markup)
+        active_users = get_all_active_users()
+        if active_users:
+            for user in active_users:
+                self.bot.send_message(user['id'], 'Wird der Waschvorgang länger oder kürzer als eine Stunde laufen ?',
+                                      reply_markup=reply_markup)
 
     def selected_button(self, update: Update, _: CallbackContext) -> None:
         query = update.callback_query
         query.answer()
         query.edit_message_text(text=f"Selected option: {query.data}")
-
