e942f1be0e9a707892996a9a68bc139c06f8caca
Korbi <sven.a.korbmacher@gmail.com>
2021-04-08 12:21:18 +0200

    -update tplink customer plugin - change value resolution of read_power_mw to round(x,1)

--- src/dataAggregator/customerTpLink/pluginTpLink.py
+++ src/dataAggregator/customerTpLink/pluginTpLink.py
@@ -4,12 +4,12 @@
 # imports
 import asyncio
 import logging
-# import kasa Lib dependencies
 from kasa import Discover
 from kasa import SmartDevice
+from src.chillyLogger import get_logger
 
-_LOGGER = logging.getLogger(__name__)
-
+# Globals
+_LOGGER = get_logger(__file__)
 detected_device = False
 detected_device_ip = False
 detected_device_is_valid = False
@@ -19,30 +19,25 @@ def init_dev_by_alias(search_alias):
     global detected_device
     global detected_device_ip
     global detected_device_is_valid
-    '''
+    """
     Search based on the configured alias the device in network.
     Create link between search_alias and plug IP address to access the values
     :return: IP address to plug
-    '''
+    """
     ip_to_return = False
     logging.basicConfig(level=logging.INFO)
-    loop = asyncio.get_event_loop()
 
-    async def _on_device(dev):
-        await dev.read_value_from_device_ip()
-        _LOGGER.info("Got device: %s", dev)
-
-    devices = loop.run_until_complete(Discover.discover(on_discovered=_on_device))
+    devices = asyncio.run(Discover.discover())
     for ip, dev in devices.items():
         if dev.alias == search_alias:
             ip_to_return = ip
     detected_device_ip = ip_to_return
-    print("detected device IP", detected_device_ip)
+    _LOGGER.info("detected device IP: "+str(detected_device_ip))
 
-    '''
+    """
     create the object_device based on the detected ip
     object provides the access to the plug
-    '''
+    """
     detected_device = SmartDevice(detected_device_ip)
     asyncio.run(detected_device.update())
     if detected_device.has_emeter:
@@ -50,9 +45,9 @@ def init_dev_by_alias(search_alias):
 
 
 def get_dev_value():
-    '''
+    """
     :return: returns the actual emeter value
-    '''
+    """
     # Globals
     global detected_device
     global detected_device_ip
@@ -68,15 +63,15 @@ def get_dev_value():
 
 
 def get_device_valid():
-    '''
+    """
     false device is not usable
     True device is ready to read the emeter values
-    '''
+    """
     return detected_device_is_valid
 
 
 def get_device_ip():
-    '''
+    """
     :return: the device IP address
-    '''
+    """
     return detected_device_ip
